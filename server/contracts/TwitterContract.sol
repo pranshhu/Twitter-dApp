e5b5a3a1bf02a71b3e4c7cfb30bee3a8f9475c2c
pranshhu <pranshu11111@gmail.com>
2022-08-04 13:45:41 +0530

    0.1.1

--- server/contracts/TwitterContract.sol
+++ server/contracts/TwitterContract.sol
@@ -3,4 +3,33 @@ pragma solidity ^0.8.4;
 
 contract TwitterContract {
 
+    event AddTweet(address recipient, uint tweetId);
+
+    struct Tweet {
+        uint id;
+        address username;
+        string tweetText;
+        bool isDeleted;
+    }
+
+    Tweet[] private tweets;
+
+    mapping (uint256 => address) tweetToOwner;
+
+    function addTweet(string memory tweetText, bool isDeleted) external {
+        uint tweetId = tweets.length;
+        tweets.push(Tweet(tweetId, msg.sender, tweetText, isDeleted));
+        tweetToOwner[tweetId] = msg.sender;
+        emit AddTweet(msg.sender, tweetId);
+    }
+
+    function getAllTweets() external view returns (Tweet[] memory) {
+        Tweet[] memory temporary = new Tweet[](tweets.length);
+
+        uint counter = 0;
+        for() {
+
+        }
+    }
+
 }
